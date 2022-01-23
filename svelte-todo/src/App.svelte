0f5f4b8fb8dd72c8fd1af6706485e2bc38f8fa51
유민호 <wingedcompany@gmail.com>
2022-01-23 12:56:48 +0900

    svelte todo 시작 3

--- svelte-todo/src/App.svelte
+++ svelte-todo/src/App.svelte
@@ -1,30 +1,54 @@
 <script>
-	export let name;
+    export let name;
+    let toggle = false;
+    let fruits = ['Apple', 'Banana', 'Cherry'];
+    const deleteFruit = () => {fruits = fruits.slice(1)};
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <h1>Hello {name}!</h1>
+    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
-
+<label>
+    <input type="text" bind:value={name}/>
+</label>
+<button on:click={()=> name = '메롱'}>
+    메롱
+</button>
+<button on:click={() => {toggle = !toggle}}>
+    토글
+</button>
+{#if toggle}
+    <div>what?</div>
+{:else}
+    <div>what!</div>
+{/if}
+<ul>
+    {#each fruits as fruit}
+        <li>{fruit}</li>
+    {/each}
+</ul>
+<button on:click={deleteFruit}>
+    Eat it!
+</button>
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
