8752b843aad11dd7069dce66f7790b282c04fde7
유민호 <wingedcompany@gmail.com>
2022-02-02 01:06:45 +0900

    dohee todo 시작 9 가즈아

--- dohee-todo/src/components/CounterText.vue
+++ dohee-todo/src/components/CounterText.vue
@@ -1,17 +1,18 @@
 <template>
-  <div>
-    {{$store.getters.count}}
+  <div>메롱
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { Vue } from 'vue-property-decorator'
+import { Getter } from 'vuex-class'
 
 export default class CounterText extends Vue {
+  // count = 0
+   @Getter('foo') readonly count!:number;
 
+   mounted () {
+     console.log('aaaaaa', this.count)
+   }
 }
 </script>
-
-<style scoped>
-
-</style>
